6cb26f084903005c7a70127ee81f3406c0fc2efb
thiagoclrss <thiagocolaress77@gmail.com>
2023-09-23 15:52:42 -0300

    starting animation

--- src/GUI/Display.java
+++ src/GUI/Display.java
@@ -9,24 +9,25 @@ public class Display extends JPanel implements ActionListener {
 
     final int PANEL_WIDTH = 690;
     final int PANEL_HEIGHT = 370;
-    Image background, home, bar;
+    Image background, home, bar, customer;
     //Image customer;
     int x=0;
     Timer timer;
     public Display(){
-        //
-        //x= 1;
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         //this.setLayout(new FlowLayout());
         this.setBackground(Color.BLACK);
         background = new ImageIcon("assets/cityBackground2.jpg").getImage();
         background = background.getScaledInstance(690,370, Image.SCALE_SMOOTH);
 
-        home = new ImageIcon("assets/house.jpg").getImage();
+        home = new ImageIcon("assets/newHouse.png").getImage();
         home = home.getScaledInstance(180,180,Image.SCALE_SMOOTH);
 
         bar = new ImageIcon("assets/restaurant.png").getImage();
         bar = bar.getScaledInstance(180,180,Image.SCALE_SMOOTH);
+
+        customer = new ImageIcon("assets/hatGuy.png").getImage();
+        customer = customer.getScaledInstance(150,150,Image.SCALE_SMOOTH);
 
         timer = new Timer(10,this);
         timer.start();
@@ -37,7 +38,8 @@ public class Display extends JPanel implements ActionListener {
 
         g2D.drawImage(background,0,0, this);
         g2D.drawImage(bar, 495,385 - bar.getHeight(null), null);
-        g2D.drawImage(home, 20, 385 - home.getHeight(null), null);
+        g2D.drawImage(home, 20, 390 - home.getHeight(null) , null);
+        g2D.drawImage(customer, 150,260,null);
     }
 
     @Override
